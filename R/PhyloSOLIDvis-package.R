acac60733de4619d7bb8b129a70cb65b57b25442
TsingYang1112 <yangqing@westlake.edu.cn>
2026-06-11 00:42:34 +0800

    Add svglite and missing dependencies to DESCRIPTION

--- R/PhyloSOLIDvis-package.R
+++ R/PhyloSOLIDvis-package.R
@@ -16,10 +16,21 @@
 #' @import dplyr
 #' @import tidyr
 #' @import converTree
-#' @importFrom ape as.phylo rotateConstr
+#' @import svglite
+#' @importFrom ape as.phylo rotateConstr Ntip Nnode
 #' @importFrom cowplot get_legend plot_grid
 #' @importFrom ggnewscale new_scale_fill
-#' @importFrom stringr str_c str_count str_split
+#' @importFrom grDevices rgb colorRampPalette
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom scales rescale
+#' @importFrom stringr str_c str_count str_split str_detect
+#' @importFrom tibble column_to_rownames as_tibble
 #' @importFrom utils packageVersion read.table write.table
+#' @importFrom stats na.omit
 ## usethis namespace: end
 NULL
+
+#' @keywords internal
+`%||%` <- function(x, y) {
+  if (is.null(x)) y else x
+}
